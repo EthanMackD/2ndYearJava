823b5c42a774a18c631e17d3b79dc5d69b99713d
EthanMackD <G00355252@atu.ie>
2025-11-26 19:17:44 +0000

    Add files via upload
    Computer Factory Java Project

---
+++ ComputerFactory/src/test/java/com/Product.java
@@ -0,0 +1,17 @@
+package test.java.com;
+
+public class Product {
+
+	private String model;
+	private String manufacturer;
+	private int width;
+	private int depth;
+	private int height;
+
+	public Product(String model, String manufacturer) {
+		super();
+		this.model = model;
+		this.manufacturer = manufacturer;
+	}
+
+}
